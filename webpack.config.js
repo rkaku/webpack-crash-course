c873d4778950bb72ca355ce70d8a39dce82d65a1
rkaku <greenapple728@icloud.com>
2019-09-20 16:38:06 +0900

    Implement sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: outputPath,
+    path: outputPath
   },
   module: {
     rules: [
@@ -13,7 +13,15 @@ module.exports = {
         test: /\.css$/,
         use: [
           'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
           'css-loader',
+          'sass-loader'
         ]
       },
       {
